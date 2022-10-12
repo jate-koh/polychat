80756d5e1877e195d5bb5da728384af7cdcca754
jate-koh <jate_ben@hotmail.com>
2022-10-12 19:09:07 +0700

    feat: attempt at connecting db (not done)

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{ useState, useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { db } from './data/firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [users, setUsers] = useState([])
+    const collectionRef = collection(db, "testdb")
+  
+    console.log(process.env.REACT_APP_PROJECT_ID);
+
+    useEffect( () => {
+      const getUsers = async () => {
+        const data = await getDocs(collectionRef)
+        setUsers(data.docs.map( (doc) => ({...doc.data(), id: doc.id})))
+      }
+  
+      getUsers();
+    }, [])
+  
+    return (
+      // <View style={styles.container}>
+      //   <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      // </View>
+      <View>
+        {users.map( (user) => {
+          return(
+            <Text>Name: {user.firstname}</Text>
+          );
+        } 
+      )}
+      </View>
+    );
+  }
+  
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 16,
+    },
 });
